8e658a0a84c6db8f0027b510bba51b7acba50807
HHarrell <hharrell@hey-kiddo.com>
2023-11-30 16:10:55 -0500

    Initial commit

---
+++ Landscape.py
@@ -0,0 +1,11 @@
+class Landscape:
+    x = 5
+
+    class Polygon:
+        def __init__(self, edges):
+            self.edges = edges
+
+    class Edge:
+        def __init__(self, v1, v2):
+            self.v1 = v1
+            self.v2 = v2
