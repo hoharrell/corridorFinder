f32225861150cd26990700091b4a863aead4ce0f
HHarrell <hharrell@hey-kiddo.com>
2023-12-01 16:14:02 -0500

    Initial commit

--- Landscape.py
+++ Landscape.py
@@ -1,11 +1,28 @@
 class Landscape:
     x = 5
 
-    class Polygon:
-        def __init__(self, edges):
-            self.edges = edges
+    def __init__(self, polygons):
+        self.polygons = polygons
 
-    class Edge:
-        def __init__(self, v1, v2):
-            self.v1 = v1
-            self.v2 = v2
+
+class Polygon:
+    def __init__(self, edges):
+        self.edges = edges
+
+
+class Edge:
+    def __init__(self, v1, v2):
+        self.v1 = v1
+        self.v2 = v2
+
+
+class Corridor:
+    def __init__(self, polygons):
+        self.polygons = polygons
+        self.width = 0
+        self.length = 0
+
+
+class Agent:
+    def __init__(self, diameter):
+        self.diameter = diameter
